e0a7a18633c66c2a850d4a6c8156a07e8884f5da
Askild <askild.tonne@tine.no>
2018-04-07 10:12:11 +0200

    Refactor out babb menu

--- babb/babb.js
+++ babb/babb.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var fs = require('fs');
+
+var menu = require('./menu');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -63,68 +65,6 @@ function getRandomFeedback() {
   return randomFeedback[Math.floor(Math.random()*randomFeedback.length)];
 }
 
-var menu = [
-  {
-    number: 1,
-    text: 'Kebab i Pita',
-    price: 95
-  },
-  {
-    number: 2,
-    text: 'Hawaii Kebab',
-    price: 100
-  },
-  {
-    number: 3,
-    text: 'Kyllingkebab i Pita',
-    price: 100
-  },
-  {
-    number: 4,
-    text: 'Kebab Rull',
-    price: 105
-  },
-  {
-    number: 5,
-    text: 'Hawaii Rull',
-    price: 110
-  },
-  {
-    number: 6,
-    text: 'Kyllingkebab Rull',
-    price: 110
-  },
-  {
-    number: 7,
-    text: 'Gresk Kebab Rull m/feta',
-    price: 110
-  },
-  {
-    number: 8,
-    text: 'Super Kebab Rull',
-    price: 115
-  },
-  {
-    number: 9,
-    text: 'Kebab Tallerken',
-    price: 115
-  },
-  {
-    number: 10,
-    text: 'Kyllingkebab Tallerken',
-    price: 120
-  },
-  {
-    number: 11,
-    text: 'Hawaii Kebab Tallerken',
-    price: 120
-  },
-  {
-    number: 12,
-    text: 'Gresk Kebab Tallerken m/feta',
-    price: 120
-  }
-];
 
 var bestillingsRegEx = /^(\d+)(mild|medium|sterk|mediumpluss)$/;
 
